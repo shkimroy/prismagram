4f21cf1110d22b94ed701d90b47e72af163bcfef
shkim <shkim.roy.d@gmail.com>
2019-06-07 00:53:03 +0900

    passport, jwt

--- src/server.js
+++ src/server.js
@@ -1,12 +1,16 @@
-require("dotenv").config()
+import "./env";
 import { GraphQLServer } from "graphql-yoga";
 import logger from "morgan";
 import schema from "./schema";
+import "./passport";
+import { authenticateJwt } from "./passport";
+
 
 const PORT = process.env.PORT || 4000;
 
-const server = new GraphQLServer({ schema });
+const server = new GraphQLServer({ schema, context: ({ request }) => ({ request }) });
 
 server.express.use(logger("dev"))
+server.express.use(authenticateJwt)
 
 server.start({ port: PORT }, () => console.log(`😃 Server running on port http://localhost:${PORT}`))
