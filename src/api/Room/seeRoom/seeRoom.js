5e1b71d693425183e33f4207356bf5d497e8d78a
shkim <shkim.roy.d@gmail.com>
2019-06-08 00:40:34 +0900

    see room

---
+++ src/api/Room/seeRoom/seeRoom.js
@@ -0,0 +1,21 @@
+import { prisma } from "../../../../generated/prisma-client";
+
+export default {
+  Query: {
+    seeRoom: async (_, args, { request, isAuthenticated }) => {
+      isAuthenticated(request);
+      const { user } = request;
+      const { id } = args;
+      const canSee = await prisma.$exists.room({
+        participants_some: {
+          id: user.id
+        }
+      })
+      if (canSee) {
+        return await prisma.room({ id });
+      } else {
+        throw Error("You can't see this")
+      }
+    }
+  }
+}
